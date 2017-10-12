0d36d24658f88aac2756e01b3e9c9a0973c87081
tha112 <tha112>
2017-10-13 12:50:33 +1300

    very basic sound testing

---
+++ testing sound/sound.h
@@ -0,0 +1,15 @@
+/** @file   sound.h
+    @author Theo Harbers
+    @date   October 2017
+    @brief  Sound Header
+*/
+
+#include "stystem.h"
+#include "pio.h"
+
+// Initialise sound ports
+void sound_init (void);
+
+// Beeps
+void beep (void);
+#endif
