2b42389748a41c21ff04be03622b7ca6f5b2257b
tha112 <tha112>
2017-10-16 10:51:53 +1300

    sound header files

--- sound.c
+++ sound.c
@@ -1,20 +1,21 @@
-#include "stystem.h"
-#include "pio.h"
 #include "sound.h"
+#include "tweeter.h"
+#include "mmelody.h"
+#include "pio.h"
 
-#define PIEZO1_PIO PIO_DEFINE (PORT_D, 4)
-#define PIEZO2_PIO PIO_DEFINE (PORT_D, 6)
+static void tweeter_task_init (void)
+{
+    tweeter = tweeter_init (&tweeter_info, TWEETER_TASK_RATE, scale_table);
 
-void sound_init (void) {
-    
     pio_config_set (PIEZO1_PIO, PIO_OUTPUT_LOW);
-    pio_config_set (PIEZO2_PIO, PIO_OUTPUT_HIGH);
-    
+    pio_config_set (PIEZO2_PIO, PIO_OUTPUT_LOW);
+
 }
 
-void beep (void) {
-    
-    pio_output_toggle (PIEZO1_PIO);
-    pio_output_toggle (PIEZO2_PIO);
-    
+static void tune_task_init (void)
+{
+    melody = mmelody_init (&melody_info, TUNE_TASK_RATE, 
+			   (mmelody_callback_t) tweeter_note_play, tweeter);
+
+    mmelody_speed_set (melody, TUNE_BPM_RATE);
 }
--- sound.h
+++ sound.h
@@ -4,12 +4,48 @@
     @brief  Sound Header
 */
 
-#include "stystem.h"
+#ifndef SOUND_H
+#define SOUND_H
+
+#include "tweeter.h"
+#include "mmelody.h"
 #include "pio.h"
 
-// Initialise sound ports
-void sound_init (void);
+#define PIEZO1_PIO PIO_DEFINE (PORT_D, 4)
+#define PIEZO2_PIO PIO_DEFINE (PORT_D, 6)
 
-// Beeps
-void beep (void);
+#define TWEETER_TASK_RATE 5000
+#define TUNE_TASK_RATE 100
+#define TUNE_BPM_RATE 200
+
+static tweeter_scale_t scale_table[] = TWEETER_SCALE_TABLE (TWEETER_TASK_RATE);
+static tweeter_t tweeter;
+static mmelody_t melody;
+static mmelody_obj_t melody_info;
+static tweeter_obj_t tweeter_info;
+
+static const char menu_tune[] = {
+#include "the_front_short.mmel"
+    " :"
+};
+
+static const char christmas_tune[] = {
+#include "merry_christmas.mmel"
+    " :"
+};
+
+static const char victory_tune[] = {
+    "ACCGF:"
+};
+
+static const char defeat_tune[] = {
+    "F///G//:"
+};
+
+// initialise tweeter task
+static void tweeter_task_init (void)
+
+// initialise tune task
+static void tune_task_init (void)
+
 #endif
