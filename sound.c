08b06504e2d7e924bd14de1952bd264b6e4de145
George K <gkh36@uclive.ac.nz>
2017-10-20 12:41:04 +1300

    Commenting

--- sound.c
+++ sound.c
@@ -1,3 +1,9 @@
+/** @file   sound.c
+    @author Theo Harbers
+    @date   October 2017.
+    @brief  Implementation of sound.
+*/
+
 #include "sound.h"
 #include "tweeter.h"
 #include "mmelody.h"
@@ -6,7 +12,7 @@
 
 tweeter_scale_t scale_table[] = TWEETER_SCALE_TABLE (TWEETER_TASK_RATE); 
 
-
+//Below are the tunes used.
 const char menu_tune[] = {
 #include "the_front.mmel"
     " :"
@@ -25,6 +31,7 @@ const char defeat_tune[] = {
     "F///G//:"
 };
 
+//Initialise the pio pins
 void tweeter_task_init (void)
 {
     tweeter = tweeter_init (&tweeter_info, TWEETER_TASK_RATE, scale_table);
@@ -34,6 +41,7 @@ void tweeter_task_init (void)
 
 }
 
+//See the tune and speed
 void tune_task_init (void)
 {
     melody = mmelody_init (&melody_info, TUNE_TASK_RATE,
